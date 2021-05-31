0d223a53f8729bf5a3b782ec40c6f9177994ea24
Aleksey Efimov <factorng@yandex.ru>
2021-05-31 16:58:29 +0300

    input zoom in iphone fixes

--- src/store/store.js
+++ src/store/store.js
@@ -19,8 +19,8 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 //const store = createStore(rootReducer, initialState, compose(applyMiddleware(thunk)));
